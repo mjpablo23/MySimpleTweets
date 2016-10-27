0c1b4ed1f57fb68c37319b01476fb22d144d18c5
mjpablo23 <mjpablo23@gmail.com>
2016-10-27 16:34:17 -0700

    working with relative timestamp, formatted correctly

--- app/src/main/java/com/codepath/apps/mysimpletweets/TweetsArrayAdapter.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/TweetsArrayAdapter.java
@@ -1,6 +1,7 @@
 package com.codepath.apps.mysimpletweets;
 
 import android.content.Context;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,7 +12,10 @@ import android.widget.TextView;
 import com.codepath.apps.mysimpletweets.models.Tweet;
 import com.squareup.picasso.Picasso;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Date;
 
 // take tweet objects and turn them into views displayed in list
 public class TweetsArrayAdapter extends ArrayAdapter<Tweet> {
@@ -35,14 +39,39 @@ public class TweetsArrayAdapter extends ArrayAdapter<Tweet> {
         ImageView ivProfileImage = (ImageView) convertView.findViewById(R.id.ivProfileImage);
         TextView tvUserName = (TextView) convertView.findViewById(R.id.tvUserName);
         TextView tvBody = (TextView) convertView.findViewById(R.id.tvBody);
+        TextView tvName = (TextView) convertView.findViewById(R.id.name);
+        TextView timestamp = (TextView) convertView.findViewById(R.id.relativeTimestamp);
 
         // 4. populate data into the subviews
         tvUserName.setText("");
-        tvUserName.setText(tweet.getUser().getScreenName());
+        tvUserName.setText("@" + tweet.getUser().getScreenName());
+        tvName.setText(tweet.getUser().getName());
+        timestamp.setText(getRelativeTime(tweet.getCreatedAt()));
         tvBody.setText(tweet.getBody());
         ivProfileImage.setImageResource(android.R.color.transparent);  // clear out the old image for a recycled view
         Picasso.with(getContext()).load(tweet.getUser().getProfileImageUrl()).into(ivProfileImage);
         // 5. return the view to be inserted into the list
         return convertView;
+    }
+
+    public String getRelativeTime(String createdAt) {
+        // http://stackoverflow.com/questions/25350164/java-parsing-twitters-created-at-string
+        System.out.println("created_at: " + createdAt);
+        final String TWITTER = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+        SimpleDateFormat sf = new SimpleDateFormat(TWITTER);
+        Date date = new Date();
+        try {
+            date = sf.parse(createdAt);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        // long now = System.currentTimeMillis();
+        long past = date.getTime();
+        // DateUtils.getRelativeDateTimeString(, now, 0L, DateUtils.FORMAT_ABBREV_ALL);
+        String pastStr = DateUtils.getRelativeTimeSpanString(past).toString();
+        String[] arr = pastStr.split("\\s+");
+        String unit = arr[1].substring(0,1);
+        String retStr = arr[0] + "" + unit;
+        return retStr;
     }
 }
