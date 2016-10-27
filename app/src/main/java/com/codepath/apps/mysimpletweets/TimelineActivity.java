083087fbec32b9e5c2c0fe4e292fdeb3e67b381f
mjpablo23 <mjpablo23@gmail.com>
2016-10-27 09:48:06 -0700

    first commit

---
+++ app/src/main/java/com/codepath/apps/mysimpletweets/TimelineActivity.java
@@ -0,0 +1,26 @@
+package com.codepath.apps.mysimpletweets;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class TimelineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+}
